69b4ada4816912624bf8620a492f6b226466c0a2
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-10-27 23:55:53 +0200

    Refactorin styles, About UI and RWD

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import globalStyle from "../../styles/Globals.module.scss";
+import bookImg from "../assets/bg-books.jpg";
 
 const About: React.FC = () => {
-  return <div>About</div>;
+  return (
+    <main className={globalStyle.container}>
+      <div className={`${globalStyle.textContainer} ${globalStyle.aboutBgImg}`}>
+        <h2 className={globalStyle.sectionTitle}>O mnie</h2>
+        <ul className={globalStyle.description}>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.sed do eiusmod tempor incididunt ut labore et</li>
+          <li>dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </li>
+          <li>exercitation ullamco laboris nisi ut aliquip ex ea commodo </li>
+          <li>consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa</li>
+          <li>qui officia deserunt mollit anim id est laborum</li>
+        </ul>
+      </div>
+      <div className={globalStyle.backgroundImg}>
+        <img src={bookImg} alt="Person with books" />
+      </div>
+    </main>
+  );
 };
 
 export default About;
